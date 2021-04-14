9683abe53e32057a9c28b74fb8af87d9250aa992
Duongds <buiduong76021@gmail.com>
2021-04-14 21:42:42 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::resource('booking', \App\Http\Controllers\API\BookingController::class);
+Route::resource('cinema', \App\Http\Controllers\API\CinemaController::class);
+Route::get('/test', [\App\Http\Controllers\API\CinemaController::class, 'searchFromRoom']);
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', [\App\Http\Controllers\API\AuthController::class, 'login']);
+    Route::post('/signup', [\App\Http\Controllers\API\AuthController::class, 'signup']);
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('/logout', [\App\Http\Controllers\API\AuthController::class, 'logout']);
+        Route::get('/user', [\App\Http\Controllers\API\AuthController::class, 'getUser']);
+    });
+});
