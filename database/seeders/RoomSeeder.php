a3229ef657985f46b349039fc942af81ca8bc9b8
duongds <buiduong76021@gmail.com>
2021-05-15 14:23:34 +0700

    seeder room

--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Room;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoomSeeder extends Seeder
 {
@@ -14,5 +15,13 @@ class RoomSeeder extends Seeder
      */
     public function run()
     {
+        $room = [
+            [
+                'name' => 'Phong so 1',
+                'seats' => [json_encode(array('hang' => 'D','cot' => 4,'status' => 1,'tpye' => 'normal')),json_encode(array('hang' => 'D', 'cot' => 4, 'status' => 1, 'tpye' => 'normal')),
+                ]
+            ]
+        ];
+        DB::table('rooms')->insert($room);
     }
 }
